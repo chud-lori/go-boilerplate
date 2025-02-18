37776b41cb38440d47583e5b456dc8a7e0d8b61d
chud-lori <imlori000@gmail.com>
2025-02-18 23:39:13 +0700

    feat: move transaction in service layer

--- domain/ports/user_repository.go
+++ domain/ports/user_repository.go
@@ -7,9 +7,9 @@ import (
 )
 
 type UserRepository interface {
-	Save(ctx context.Context, user *entities.User) (*entities.User, error)
-	Update(ctx context.Context, user *entities.User) (*entities.User, error)
-	Delete(ctx context.Context, id string) error
-	FindById(ctx context.Context, id string) (*entities.User, error)
-	FindAll(ctx context.Context) ([]*entities.User, error)
+	Save(ctx context.Context, tx Transaction,user *entities.User) (*entities.User, error)
+	Update(ctx context.Context, tx Transaction, user *entities.User) (*entities.User, error)
+	Delete(ctx context.Context, tx Transaction, id string) error
+	FindById(ctx context.Context, tx Transaction, id string) (*entities.User, error)
+	FindAll(ctx context.Context, tx Transaction) ([]*entities.User, error)
 }
