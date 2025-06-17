c4d21b2b173135c688af82d105c4619708e306f1
chud-lori <imlori000@gmail.com>
2025-06-18 00:30:43 +0700

    user entitiy property naming

--- domain/entities/user.go
+++ domain/entities/user.go
@@ -2,11 +2,9 @@ package entities
 
 import "time"
 
-
 type User struct {
-    Id          string `json:"id"`
-    Email       string `json:"email"`
-    Passcode    string `json:"-"`
-    Created_at  time.Time `json:"created_at"`
+	Id        string    `json:"id"`
+	Email     string    `json:"email"`
+	Passcode  string    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
 }
-
