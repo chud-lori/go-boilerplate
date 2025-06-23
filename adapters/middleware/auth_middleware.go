7a7d0968a707cfefe2e0427f60abbc3fa88d4825
chud-lori <imlori000@gmail.com>
2025-06-23 10:40:16 +0700

    add procted route auth middleware

--- adapters/middleware/auth_middleware.go
+++ adapters/middleware/auth_middleware.go
@@ -13,29 +13,35 @@ type contextKey string
 
 const UserIDKey contextKey = "userID"
 
-func JWTMiddleware(tokenManager ports.TokenManager, logger *logrus.Logger) func(http.Handler) http.Handler {
+func JWTMiddleware(next http.Handler, tokenManager ports.TokenManager, logger *logrus.Logger) http.Handler {
 	mwLogger := logger.WithFields(logrus.Fields{
 		"layer": "middleware",
 	})
 
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			authHeader := r.Header.Get("Authorization")
-			if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
-				http.Error(w, "Unauthorized: missing token", http.StatusUnauthorized)
-				return
-			}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-			token := strings.TrimPrefix(authHeader, "Bearer ")
-			userID, err := tokenManager.ValidateToken(token)
-			if err != nil {
-				mwLogger.Warnf("Invalid token: %v", err)
-				http.Error(w, "Unauthorized: invalid token", http.StatusUnauthorized)
-				return
-			}
+		// only /api/user since middleware not works
+		// TODO: find a way to grouped middleware routing
+		if !strings.HasPrefix(r.URL.Path, "/api/user") {
+			next.ServeHTTP(w, r)
+			return
+		}
 
-			ctx := context.WithValue(r.Context(), UserIDKey, userID)
-			next.ServeHTTP(w, r.WithContext(ctx))
-		})
-	}
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			http.Error(w, "Unauthorized: missing token", http.StatusUnauthorized)
+			return
+		}
+
+		token := strings.TrimPrefix(authHeader, "Bearer ")
+		userID, err := tokenManager.ValidateToken(token)
+		if err != nil {
+			mwLogger.Warnf("Invalid token: %v", err)
+			http.Error(w, "Unauthorized: invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), UserIDKey, userID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
