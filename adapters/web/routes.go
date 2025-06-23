0a8530940ee65202c411d0032c47a5fb7fc4109a
chud-lori <imlori000@gmail.com>
2025-06-23 10:40:40 +0700

    auth routes

--- adapters/web/routes.go
+++ adapters/web/routes.go
@@ -13,3 +13,8 @@ func UserRouter(controller ports.UserController, serve *http.ServeMux) {
 	serve.HandleFunc("GET /api/user/{userId}", controller.FindById)
 	serve.HandleFunc("GET /api/user", controller.FindAll)
 }
+
+func AuthRouter(controller ports.AuthController, serve *http.ServeMux) {
+	serve.HandleFunc("POST /api/signin", controller.SignIn)
+	serve.HandleFunc("POST /api/signup", controller.SignUp)
+}
