fe356f0e69c0f5243a544fc8a9c91a8eb9ccad0c
chud-lori <imlori000@gmail.com>
2025-06-23 10:45:26 +0700

    auth service

---
+++ domain/services/auth_service_impl.go
@@ -0,0 +1,112 @@
+package services
+
+import (
+	"context"
+
+	"time"
+
+	"github.com/chud-lori/go-boilerplate/domain/entities"
+	"github.com/chud-lori/go-boilerplate/domain/ports"
+	appErrors "github.com/chud-lori/go-boilerplate/pkg/errors"
+	"github.com/chud-lori/go-boilerplate/pkg/logger"
+	"github.com/sirupsen/logrus"
+)
+
+type AuthServiceImpl struct {
+	DB ports.Database
+	ports.UserRepository
+	ports.Encryptor
+	ports.TokenManager
+	CtxTimeout time.Duration
+}
+
+func (s *AuthServiceImpl) SignIn(c context.Context, user *entities.User) (*entities.User, string, error) {
+	logger, _ := c.Value(logger.LoggerContextKey).(logrus.FieldLogger)
+	ctx, cancel := context.WithTimeout(c, s.CtxTimeout)
+	defer cancel()
+
+	tx, err := s.DB.BeginTx(ctx)
+	if err != nil {
+		logger.WithError(err).Error("Failed to begin transaction")
+		return nil, "", err
+	}
+
+	defer func() {
+		if r := recover(); r != nil || err != nil {
+			tx.Rollback()
+		}
+	}()
+
+	foundUser, err := s.UserRepository.FindByEmail(ctx, tx, user.Email)
+	if err != nil {
+		logger.WithError(err).Warn("User not found by email")
+		return nil, "", appErrors.NewNotFoundError("Unauthorized", err)
+	}
+
+	if err := s.Encryptor.CompareHash(foundUser.Password, user.Password); err != nil {
+		logger.WithError(err).Warn("Invalid password")
+		return nil, "", appErrors.NewNotFoundError("Unauthorized", err)
+	}
+
+	token, err := s.TokenManager.GenerateToken(foundUser.Id)
+	if err != nil {
+		logger.WithError(err).Error("Failed to generate token")
+		return nil, "", err
+	}
+
+	if err = tx.Commit(); err != nil {
+		logger.WithError(err).Error("Failed to commit transaction")
+		return nil, "", err
+	}
+
+	return foundUser, token, err
+}
+
+func (s *AuthServiceImpl) SignUp(c context.Context, user *entities.User) (*entities.User, string, error) {
+	logger, _ := c.Value(logger.LoggerContextKey).(logrus.FieldLogger)
+	ctx, cancel := context.WithTimeout(c, s.CtxTimeout)
+	defer cancel()
+
+	tx, err := s.DB.BeginTx(ctx)
+	if err != nil {
+		logger.WithError(err).Error("Failed to begin transaction")
+		return nil, "", err
+	}
+
+	defer func() {
+		if r := recover(); r != nil || err != nil {
+			tx.Rollback()
+		}
+	}()
+
+	existing, _ := s.UserRepository.FindByEmail(ctx, tx, user.Email)
+	if existing != nil {
+		logger.Warn("Email already in use")
+		return nil, "", appErrors.NewBadRequestError("Email already exist", err)
+	}
+
+	hashedPassword, err := s.Encryptor.HashPassword(user.Password)
+	if err != nil {
+		logger.WithError(err).Error("Failed to hash password")
+		return nil, "", err
+	}
+	user.Password = hashedPassword
+
+	if _, err := s.UserRepository.Save(ctx, tx, user); err != nil {
+		logger.WithError(err).Error("Failed to create user")
+		return nil, "", err
+	}
+
+	token, err := s.TokenManager.GenerateToken(user.Id)
+	if err != nil {
+		logger.WithError(err).Error("Failed to generate token")
+		return nil, "", err
+	}
+
+	if err = tx.Commit(); err != nil {
+		logger.WithError(err).Error("Failed to commit transaction")
+		return nil, "", err
+	}
+
+	return user, token, nil
+}
