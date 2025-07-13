4ffa52365e546d1abfdc8b3281a5a2218d28b573
chud-lori <imlori000@gmail.com>
2025-07-13 11:19:44 +0700

    setup redis test

---
+++ internal/testutils/setup_redis.go
@@ -0,0 +1,32 @@
+package testutils
+
+import (
+	"context"
+	"time"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func SetupRedisContainer(ctx context.Context) (testcontainers.Container, string, error) {
+	req := testcontainers.ContainerRequest{
+		Image:        "redis:7-alpine",
+		ExposedPorts: []string{"6379/tcp"},
+		WaitingFor:   wait.ForListeningPort("6379/tcp").WithStartupTimeout(10 * time.Second),
+	}
+	redisC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		return nil, "", err
+	}
+
+	endpoint, err := redisC.Endpoint(ctx, "")
+	if err != nil {
+		redisC.Terminate(ctx)
+		return nil, "", err
+	}
+
+	return redisC, endpoint, nil
+}
