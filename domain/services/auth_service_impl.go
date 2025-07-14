b2c1b52279b87410db6b412df4066824a8746beb
chud-lori <imlori000@gmail.com>
2025-07-14 10:31:10 +0700

    impl api call on auth service

--- domain/services/auth_service_impl.go
+++ domain/services/auth_service_impl.go
@@ -5,6 +5,8 @@ import (
 
 	"time"
 
+	"encoding/json"
+
 	"github.com/chud-lori/go-boilerplate/domain/entities"
 	"github.com/chud-lori/go-boilerplate/domain/ports"
 	appErrors "github.com/chud-lori/go-boilerplate/pkg/errors"
@@ -18,7 +20,8 @@ type AuthServiceImpl struct {
 	ports.Encryptor
 	ports.TokenManager
 	ports.MailService
-	CtxTimeout time.Duration
+	ExternalApi ports.ExternalApiClient
+	CtxTimeout  time.Duration
 }
 
 func (s *AuthServiceImpl) SignIn(c context.Context, user *entities.User) (*entities.User, string, error) {
@@ -60,11 +63,33 @@ func (s *AuthServiceImpl) SignIn(c context.Context, user *entities.User) (*entit
 		return nil, "", err
 	}
 
+	// Disable throwing error, as this dummy
+	// External API call POST dummy
+	auditPayload := map[string]interface{}{
+		"user_id": foundUser.ID.String(),
+		"event":   "sign_in",
+		"time":    time.Now().Format(time.RFC3339),
+	}
+	body, _ := json.Marshal(auditPayload)
+	postResp, errApi := s.ExternalApi.DoRequest(ctx, "POST", "https://dummy-external-api.local/audit", map[string]string{"Content-Type": "application/json"}, body)
+	if errApi != nil {
+		logger.WithError(errApi).Warn("Failed to notify external audit API (POST)")
+	} else {
+		logger.Infof("POST external API response: %s", string(postResp))
+	}
+
+	// External API call GET dummy
+	getResp, errApi := s.ExternalApi.DoRequest(ctx, "GET", "https://dummy-external-api.local/userinfo?id="+foundUser.ID.String(), nil, nil)
+	if err != nil {
+		logger.WithError(errApi).Warn("Failed to get user info from external API (GET)")
+	} else {
+		logger.Infof("GET external API response: %s", string(getResp))
+	}
+
 	// Disable throwing error, as this mail service is optional
 	errMail := s.MailService.SendSignInNotification(ctx, foundUser.Email, "User logged in just now")
 	if errMail != nil {
-		logger.WithError(err).Error("Failed to send mail")
-		// return nil, "", err
+		logger.WithError(errMail).Error("Failed to send mail")
 	}
 
 	return foundUser, token, err
